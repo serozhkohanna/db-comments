4ab0ff913a7bd32560327c6ae66011af7e3a4a65
serozhkohanna <serozhkohanna@gmail.com>
2020-07-25 21:12:55 +0300

    Created common method for get data request.

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,27 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import './CommentList.scss';
-import axios from 'axios';
+import getData from "../../constants/getData";
 
 import CommentItem from "../CommentItem/CommentItem";
-
 const CommentList = () => {
   const [comments, setComments] = useState([]);
 
-  const updateList = () => {
-	axios.get('http://localhost:5000/comments')
-	  .then(({data}) => {
-		setComments(data);
-	  })
-	  .catch(err => console.log(err));
-  }
+  // const updateList = () => {
+	// axios.get('http://localhost:5000/comments')
+	//   .then(({data}) => {
+	// 	setComments(data);
+	//   })
+	//   .catch(err => console.log(err));
+  // }
 
   useEffect(() => {
-	updateList();
+	getData(setComments);
   }, [])
 
   return <section className='comment-list'>
 	{comments.map((item, i) => {
-	  return <CommentItem isUpdated={updateList} key={i} comment={item}/>
+	  return <CommentItem isUpdated={() => getData(setComments)} key={i} comment={item}/>
 	})}
   </section>
 }
